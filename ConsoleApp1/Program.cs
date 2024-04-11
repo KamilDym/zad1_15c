a07bcb0f8b75cc1632e9acb48bc62446630015dc
s25005 <s25005@pjwstk.edu.pl>
2024-04-11 18:05:53 +0200

    branch feature-new commit 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World 0-0!");
 Console.WriteLine("Hello, World ^_^!");
 Console.WriteLine("Hello, World 12345!");
 
-static double staticaverage(int[] args)
+static double staticaverage(int[] arguments)
 {
-    return args.Average();
+    return arguments.Average();
 }
 
 static double FeatureMax(int[] args)
 {
     return args.Max();
 }
+
